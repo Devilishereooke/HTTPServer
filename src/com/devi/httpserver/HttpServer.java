a8a06a8af93c810030e46c80691008ba5436379d
Devil_Alert <devidasdeo9@gmail.com>
2025-12-16 12:32:43 +0530

    Read Client Request
    Wrote a function, handleClient(), which took clientSocket as an input and then reads client request (untill the empty line, headers). And then print the lines of the request.

--- src/com/devi/httpserver/HttpServer.java
+++ src/com/devi/httpserver/HttpServer.java
@@ -13,17 +13,41 @@ public class HttpServer {
     public void start(){
         System.out.println("Starting a socket on port: " + port);
 
+        // 1. Start the socket on port
         try(ServerSocket serverSocket = new ServerSocket(port)){
             while(true){
+                // 2. Listen for the client request
                 Socket clientSocket = serverSocket.accept();
 
                 System.out.println("Client connected!!" + clientSocket);
 
+                // 3. Handle HTTP request
+                handleClient(clientSocket);
+
                 clientSocket.close();
             }
         }
-        catch(IOException e){
+        catch(IOException e){   
             System.out.println(e);
         }
+    }
+
+    private void handleClient(Socket clientSocket) throws IOException{
+        BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String line;
+        System.out.println("------- HTTP Request Start -------");
+
+        while((line = clientInput.readLine()) != null){
+            if(line.isEmpty()){
+                break;                  // end of headers
+            }
+
+            System.out.println(line);
+        }
+
+        System.out.println("------- HTTP Request End -------");
+
+        clientSocket.close();
     }
 }
